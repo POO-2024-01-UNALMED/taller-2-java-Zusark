88865fa4b5b6e625686b8fe2f39e8d11d4f9e9d5
Zusark <149200499+Zusark@users.noreply.github.com>
2024-02-17 21:08:45 -0500

    prueba 2

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -1,37 +1,35 @@
 package test;
 
 public class Auto {
-	String modelo, marca;
-	int precio, registro, cantidadCreados;
+	String modelo;
+	int precio;
 	Asiento[] asientos;
+	String marca;
 	Motor motor;
+	int registro;
+	static int cantidadCreados;
 	
 	int cantidadAsientos() {
 		int k = 0;
-		for (int i = 0;i < this.asientos.length;i++) {
+		for (int i = 0;i< asientos.length;i++) {
 			if (this.asientos[i] != null) {
 				k++;
 			}
 		}
 		return k;
 	}
-	String verficarIntegridad() {
-		boolean j;
-		for (int i = 0;i < this.asientos.length; i++) {
-			if (this.asientos[i] != null) {
-				if ((this.asientos[i].registro == this.registro)&&(this.asientos[i].registro == this.motor.registro)) {
-					j = true;
-				}
-				else {
-					j = false;
+	String verificarIntegridad() {
+		if (this.registro == this.motor.registro) {
+			for (int i = 0; i < asientos.length;i++) {
+				if (asientos[i] != null) {
+					if (asientos[i].registro != registro) {
+						return "Las piezas no son originales";
+					}
 				}
 			}
-		}
-		if ((this.registro != this.motor.registro)||(j == false)) {
-			return "Las piezas no son originales";
-		}
-		else {
 			return "Auto original";
+		}else {
+			return "Las piezas no son originales";
 		}
 	}
 }
